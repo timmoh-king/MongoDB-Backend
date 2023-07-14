a1a92b6d853b94b676da2ab5d4d47d5699dd2784
timmoh-king <timothykariuki43@gmail.com>
2023-07-14 18:09:39 +0300

    folders structure

---
+++ backend/controllers/blogs.js
@@ -0,0 +1,22 @@
+const blogRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogRouter.post("/", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+//put request
+//delete request
+//unit testing
+
+module.exports = blogRouter;
