792d14fa4f0afcea60032b98a4515752c18dbc6c
timmoh-king <timothykariuki43@gmail.com>
2023-07-18 13:32:54 +0300

    controllers/blogs.js

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -2,21 +2,59 @@ const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
 blogRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+  Blog.find({})
+    .then((blogs) => {
+      response.json(blogs);
+    })
+    .catch((error) => next(error));
+});
+
+blogRouter.get("/:id", (request, response) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog);
+      } else {
+        response.status(404);
+      }
+    })
+    .catch((error) => next(error));
 });
 
 blogRouter.post("/", (request, response) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
 
-//put request
-//delete request
-//unit testing
+blogRouter.put("/:id", (request, response) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then((updatedBlog) => {
+      response.json(updatedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+blogRouter.delete("/:id", (request, response) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
 module.exports = blogRouter;
